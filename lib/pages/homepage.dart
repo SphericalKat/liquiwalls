a21c15e0fec9d88f20973219282ab0fe72e5c3b8
ATechnoHazard <amolele@gmail.com>
2020-10-25 01:54:42 +0530

    :zap: implement sliding panel
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,18 +1,44 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {},
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Row(
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(18),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Icon(Icons.menu, size: 30),
+                ),
+                onTap: () {
+                  print("owo");
+                },
+              ),
+            )
+          ],
         ),
       ),
-      body: Center(
-        child: Text("Hello, world!"),
+      body: SlidingUpPanel(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+        maxHeight: height,
+        minHeight: height - 200,
+        body: FittedBox(
+          fit: BoxFit.cover,
+          child: Image.asset("assets/images/flowing_skies.jpg"),
+        ),
+        panel: ListTile(title: Text('Item 1')),
       ),
     );
   }
