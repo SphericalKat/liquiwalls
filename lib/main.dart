edf378a8de47563f503f4ca7ec2827deddd35e21
ATechnoHazard <amolele@gmail.com>
2020-10-26 23:07:56 +0530

    :zap: set up firebase boilerplate
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:liqui_walls/router/router.gr.dart';
@@ -12,17 +13,31 @@ class WallsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(systemTheme);
+    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'LiquiWalls',
-      builder: ExtendedNavigator.builder<CustomRouter>(
-        router: CustomRouter(),
-        builder: (context, extendedNav) => Theme(
-          data: lightTheme,
-          child: extendedNav,
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'LiquiWalls',
+              builder: ExtendedNavigator.builder<CustomRouter>(
+                router: CustomRouter(),
+                builder: (context, extendedNav) => Theme(
+                  data: lightTheme,
+                  child: extendedNav,
+                ),
+              ),
+            );
+          }
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        });
   }
 }
